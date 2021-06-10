4ac17e2a6cc4d3bac14bdc4f82c88cef5498846a
Lana <svjetlanazlatkovic@gmail.com>
2021-06-10 20:24:46 +0200

    varijable i uvjeti

---
+++ lesson_1/moj_prvi_kod.py
@@ -0,0 +1,12 @@
+#kreiranje varijabli
+broj_godina = 25
+broj_godina_1 = 1
+ukupan_broj_godina = broj_godina + broj_godina_1
+print(broj_godina)
+
+# učitavanje od korisnika
+unos_godina = input ("upisite svoj broj godina: ")
+print("Upisali ste" + unos_godina)
+
+broj_godina = int(unos_godina)
+print(broj_godina)
---
+++ lesson_1/python_novo.py
@@ -0,0 +1,6 @@
+a=20
+b=30
+if a > b:
+    print("A je veći od b")
+    if b > a:
+        print("B je veći od A")
